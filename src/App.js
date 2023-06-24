06b2a6ea28fd7572e2d5194696156315b6aa1441
ht1204 <ptb1204@hotmail.com>
2023-06-23 23:30:21 -0500

    feature: refactor class component, validations & impls unit testing

--- src/App.js
+++ src/App.js
@@ -1,24 +1,102 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+  regex_input = /^[a-zA-Z\s]+$/;
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      guests: [],
+      firstName: '',
+      lastName: '',
+      firstNameError: false,
+      lastNameError: false,
+    };
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleFirstNameChange = this.handleFirstNameChange.bind(this);
+    this.handleLastNameChange = this.handleLastNameChange.bind(this);
+  }
+
+  handleFirstNameChange(event) {
+    const { value } = event.target;
+    this.setState({
+      firstName: value,
+      firstNameError: !this.validateInput(value),
+    });
+  }
+
+  handleLastNameChange(event) {
+    const { value } = event.target;
+    this.setState({
+      lastName: value,
+      lastNameError: !this.validateInput(value),
+    });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+
+    const firstNameError = !this.validateInput(this.state.firstName);
+    const lastNameError = !this.validateInput(this.state.lastName);
+
+    if (!firstNameError && !lastNameError) {
+      const newGuest = {
+        firstName: this.state.firstName.trim(),
+        lastName: this.state.lastName.trim(),
+      };
+
+      this.setState((prevState) => ({
+        guests: [...prevState.guests, newGuest],
+        firstName: '',
+        lastName: '',
+        firstNameError: false,
+        lastNameError: false,
+      }));
+    } else {
+      this.setState({
+        firstNameError,
+        lastNameError,
+      });
+    }
+  }
+
+  validateInput(value) {
+    return value.trim() !== '' && this.regex_input.test(value);
+  }
+
   render() {
     return (
       <div className="container">
         <div className="row">
           <div className="col-sm-6 col-sm-offset-3">
-            <form>
+            <form onSubmit={this.handleSubmit}>
               <div className="form-group">
                 <label htmlFor="first-name">Nombre</label>
-                <input type="text" className="form-control" name="first-name" />
+                <input
+                  type="text"
+                  className="form-control"
+                  name="first-name"
+                  value={this.state.firstName}
+                  onChange={this.handleFirstNameChange}
+                />
               </div>
 
               <div className="form-group">
                 <label htmlFor="last-name">Apellido</label>
-                <input type="text" className="form-control" name="last-name" />
+                <input
+                  type="text"
+                  className="form-control"
+                  name="last-name"
+                  value={this.state.lastName}
+                  onChange={this.handleLastNameChange}
+                />
               </div>
 
               <div className="action">
-                <button type="submit" className="btn btn-primary">Agregar Invitado</button>
+                <button type="submit" className="btn btn-primary">
+                  Agregar Invitado
+                </button>
               </div>
             </form>
 
@@ -30,13 +108,18 @@ class App extends Component {
                 </tr>
               </thead>
               <tbody>
-
+                {this.state.guests.map((guest, index) => (
+                  <tr key={index}>
+                    <td>{guest.firstName}</td>
+                    <td>{guest.lastName}</td>
+                  </tr>
+                ))}
               </tbody>
             </table>
           </div>
         </div>
       </div>
-    )
+    );
   }
 }
 
